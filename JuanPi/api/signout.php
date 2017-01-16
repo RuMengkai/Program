76249af7113deebc55aeac5df3e3043d7751e009
茹孟凯 <874968552@qq.com>
2017-01-16 15:03:49 +0800

    top

---
+++ JuanPi/api/signout.php
@@ -0,0 +1,10 @@
+<?php
+    header("Content-type:text/html;charset=utf-8");
+    function doOut(){
+		session_start();
+		session_unset("user");
+		session_destroy();
+		echo 0;
+    }
+    doOut();
+?>
---
+++ JuanPi/js/index.js
@@ -0,0 +1,29 @@
+$(function() {
+	//show 
+	$(".bag-show").on("mouseover",function() {
+		$(".bag-tool").css("display","block");
+	});
+	$(".bag-show").on("mouseout",function() {
+		$(".bag-tool").css("display","none");
+	});
+	$(".center").on("mouseover",function() {
+		$(this).children("div").css("display","block")
+	});
+	$(".center").on("mouseout",function() {
+		$(".center div").css("display","none");
+	});
+	//手机二维码show
+	$(".navigation li:last-child").on("mouseover",function() {
+		$(".phone-in").css("display","block");
+	});
+	$(".navigation li:last-child").on("mouseout",function() {
+		$(".phone-in").css("display","none");
+	});
+	//每日签到show
+	$(".state-show").on("mouseover",function() {
+		$(".normal-side-box").css("display","block");
+	});
+	$(".state-show").on("mouseout",function() {
+		$(".normal-side-box").css("display","none");
+	});
+});
